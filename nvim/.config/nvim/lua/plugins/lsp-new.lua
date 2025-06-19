0a01b9769048a73f3a4ea68cc06b5befd36db083
Max Sherwin <maxwellsherwin@proton.me>
2025-06-19 15:20:01 -0500

    Newest work dotfile set

--- nvim/.config/nvim/lua/plugins/lsp-new.lua
+++ nvim/.config/nvim/lua/plugins/lsp-new.lua
@@ -1,67 +1,76 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true,
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local cmp_nvim_lsp = require("cmp_nvim_lsp")
-            local capabilities = vim.tbl_deep_extend(
-                "force",
-                {},
-                vim.lsp.protocol.make_client_capabilities(),
-                cmp_nvim_lsp.default_capabilities()
-            )
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				cmp_nvim_lsp.default_capabilities()
+			)
 
-            local lspconfig = require("lspconfig")
+			-- Add this inside your config function, before the lspconfig setup
+			vim.diagnostic.config({
+				virtual_text = true, -- This enables inline diagnostics
+				signs = true,
+				update_in_insert = false,
+				underline = true,
+				severity_sort = true,
+			})
 
-            lspconfig.tailwindcss.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ruby_lsp.setup({
-                capabilities = capabilities,
-                auto_start = true,
-                cmd = { "/home/mackswell/.rbenv/shims/ruby-lsp" },
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.intelephense.setup({
-                capabilities = capabilities
-            })
-            lspconfig.zls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities
-            })
-            lspconfig.rust_analyzer.setup({
-                capabilities = capabilities
-            })
-            lspconfig.jdtls.setup({
-                capabilities = capabilities
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-        end,
-    },
+			local lspconfig = require("lspconfig")
+
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ruby_lsp.setup({
+				capabilities = capabilities,
+				auto_start = true,
+                cmd = { "mise", "exec", "ruby", "--", "ruby-lsp" }
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.intelephense.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.zls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,25 +1,22 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            debug = true,
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.clang_format,
-                null_ls.builtins.formatting.prettier.with({
-                    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "json", "yaml" }, -- Apply to JS/TS files
-                }),
-                null_ls.builtins.formatting.stylelint.with({
-                    filetypes = { "scss", "css", "less" },
-                }),
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			debug = true,
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.clang_format,
+                null_ls.builtins.formatting.erb_format,
+				null_ls.builtins.formatting.prettier.with({
+					filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "json", "yaml" }, -- Apply to JS/TS files
+				}),
+			},
+		})
 
-            },
-        })
-
-        vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
-    end,
+		vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+	end,
 }
